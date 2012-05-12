56704141a8e5b92baeefcd6a7c453ebdc9dbf080
Kræn Hansen <post@creen.dk>
2012-05-12 22:16:14 +0200

    Changed the name of the project

--- src/dk/rf12/stats/RF12StatsMapActivity.java
+++ src/dk/rf12/stats/RF12StatsMapActivity.java
@@ -2,18 +2,7 @@ package dk.rf12.stats;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.RatingBar;
-import android.widget.SeekBar;
-import android.widget.Toast;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 public class RF12StatsMapActivity extends Activity {
 	
